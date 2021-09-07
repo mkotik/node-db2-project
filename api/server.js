57d63ae6e7d577a84b29b607971cc79fa73964e2
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-09-07 18:11:36 -0400

    test passed

--- api/server.js
+++ api/server.js
@@ -1,7 +1,16 @@
-const express = require("express")
+const express = require("express");
+const carsRouter = require("./cars/cars-router");
+const server = express();
 
-const server = express()
+server.use(express.json());
 
-// DO YOUR MAGIC
+server.use("/api/cars", carsRouter);
 
-module.exports = server
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
